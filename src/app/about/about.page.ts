6f9ecfa559596ac83e566cdcd2bcc2fc6a8f0d1d
Allan Browning <a.browning@griffith.edu.au>
2024-11-18 15:16:48 +1000

    updated look of quiz

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar,IonButtons,IonBackButton,IonMenuButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar,IonButtons,IonBackButton,IonList,IonItem,IonMenuButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.page.html',
   styleUrls: ['./about.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar,IonButtons,IonMenuButton, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar,IonButtons,IonMenuButton,IonList,IonItem, CommonModule, FormsModule]
 })
 export class AboutPage implements OnInit {
 
